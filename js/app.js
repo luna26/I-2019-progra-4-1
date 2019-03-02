409353aa164598d045156b2eb83b89f9ee35974d
Luna Gonzalez, Jose M <jose.m.luna.gonzalez@accenture.com>
2019-03-01 19:53:53 -0600

    form - js
    form - js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
 (function(){
     var CLASE7 = {
         init: function(){
-            
+            this.addEvents();
+            console.log('this.cacheElements', this.cacheElements.$btnSubmitForm);
+        },
+
+        addEvents: function(){
+            this.cacheElements.$btnSubmitForm.on('click', function(){
+                console.log('click');
+                this.sendInfo();
+            }.bind(this));            
+        },
+
+        cacheElements: {
+            $btnSubmitForm : $('#btn-submit-form'),
+            $inputUser : $('#input-user'),
+            $inputPassword : $('#input-pass'),
+        },
+
+        sendInfo : function(){
+            var user = this.cacheElements.$inputUser.val();
+            var pass = this.cacheElements.$inputPassword.val();
+
+            var data = {
+                user : user,
+                pass : pass
+            }
+
+            //enviamos al servidor
+            console.log('data', data);
+
+            //ajax se usa para la comunicacion cliente - servidor
+            $.ajax({
+                data: data,
+                type: 'post',
+                url: '',
+                success: function (result, status, xhr) {
+                  
+                }
+            });
         }
     }
+
+    CLASE7.init();
 })();
