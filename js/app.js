2ead8accde0d553086ac2c7ddd8d52eba04cd5f9
Luna Gonzalez, Jose M <jose.m.luna.gonzalez@accenture.com>
2019-03-13 21:01:04 -0600

    form - validate
    form - validate

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 (function(){
-    
+        // console.log('loading');
+        // $('#button').on('click', function(){
+        //     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        //     console.log('es un correo', re.test($("#email").val()));
+        // });
+
+        $("#email").on('blur', function(){
+            console.log('perdi el foco');
+            let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+            if(!re.test($("#email").val())){
+                $('.error-email').html('Ingrese un email valido');
+            }
+        });
 })();
